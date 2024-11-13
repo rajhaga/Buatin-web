b058d68252bf39921ddae7e18440715f1a9f2030
rajhaga <134823961+rajhaga@users.noreply.github.com>
2024-11-13 14:14:49 +0700

    Menyelesaikan migrations konflik

--- database/migrations/2024_10_23_143656_create_orders_table.php
+++ database/migrations/2024_10_23_143656_create_orders_table.php
@@ -6,24 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    
+    /**
+     * Run the migrations.
+     */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('packages', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('package_id')->constrained()->onDelete('cascade');
-            $table->string('location');
-            $table->string('description');
-            $table->date('deadline')->default('2024-10-25');
-            $table->string('status')->default('pending');
-            $table->string('proof')->nullable();
+            $table->string('name');
+            $table->string('category');
+            $table->text('description');
+            $table->decimal('price', 10, 2);
             $table->timestamps(0);
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('packages');
     }
 };
